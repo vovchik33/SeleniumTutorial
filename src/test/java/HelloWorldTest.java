9ada7f3064c398cab2c4ae81b2c65542fe607258
Vladimir Kravchenko <Vladimir.Kravchenko@playtech.com>
2014-12-02 18:44:18 +0200

    Initial commit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by vladimirkr on 12/2/2014.
+ */
+public class HelloWorldTest {
+    @Test
+    public void loadTestThisWebsite() {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        System.out.println("Page Title is " + driver.getTitle());
+        System.out.printf("Thread Id : %s%n", Thread.currentThread().getId());
+        Assert.assertEquals("Google", driver.getTitle());
+        driver.quit();
+
+    }
+}
